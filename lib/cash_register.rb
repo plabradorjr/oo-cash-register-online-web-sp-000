b896b01db2ffbdc1cc5732ee73da57731e81625d
Pancho Labrador <plabradorjr@gmail.com>
2019-10-26 05:28:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class CashRegister
 
-  attr_accessor :total, :discount, :total, :last_transaction
+  attr_accessor :total, :discount, :items, :last_transaction
 
 
   def initialize(discount = 0)

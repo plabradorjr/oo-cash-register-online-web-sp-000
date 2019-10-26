da37b5ec898e3870edd5acd1f9df7d9576d8fc5c
Pancho Labrador <plabradorjr@gmail.com>
2019-10-26 05:27:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, amount, quantity=1)
+  def add_item(title, amount, quantity = 1)
     self.total += amount * quantity
     quantity.times do
       items << title

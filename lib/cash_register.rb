ee1a7544f5cdc7735fdc0e30aeff278e4a81caca
Pancho Labrador <plabradorjr@gmail.com>
2019-10-26 05:27:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     quantity.times do
       @items << title
     end
-    self.last_transaction = amount * quantity
+    self.@last_transaction = amount * quantity
   end
 
   def apply_discount
